31915e51d7947bd4844acbb055524240e9a870bc
MeghanaBait <meghanawork777@gmail.com>
2024-02-27 11:16:52 +0530

    responsive section2

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const phoneRegex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
 const digitRegex = /^\d+$/;
 
 const mainForm = document.getElementById('cv-form');
+
 const validType = {
     TEXT: 'text',
     TEXT_EMP: 'text_emp',
@@ -211,7 +212,7 @@ const displayCV = (userData) => {
 const generateCV = () => {
     let userData = getUserInputs();
     displayCV(userData);
-    console.log(userData);
+    // console.log(userData);
 }
 
 function previewImage(){
